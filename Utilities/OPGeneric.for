132ba1e1eb93375572359d01596025d6ffa6ce2e
chporter <cporter@ufl.edu>
2022-02-16 20:57:17 -0500

    Retool OPGENERIC for Low Input Systems daily output format

--- Utilities/OPGeneric.for
+++ Utilities/OPGeneric.for
@@ -19,39 +19,28 @@
       IMPLICIT  NONE
       SAVE
 
+      CHARACTER*6, PARAMETER :: ERRKEY = 'GENPRN'
       CHARACTER*10, DIMENSION(12) :: FormatTxt 
-      CHARACTER*10, DIMENSION(12) :: HeaderTxt
+      CHARACTER*10, DIMENSION(12) :: HeaderTxt, DATETXT
       CHARACTER*11, PARAMETER :: OUTG1 = 'Generic.OUT'
       CHARACTER*11, PARAMETER :: OUTG2 = 'Generic.CSV'
+      CHARACTER*78, MSG(10)
       CHARACTER*220 FMT_STRING_A, FMT_STRING_C
       CHARACTER*220 HDR_String_A, HDR_String_C
       INTEGER DAS, DOY, DYNAMIC, ERRNUM, LUN1, LUN2
-      INTEGER NLayr, INCDAT, RUN, YEAR, YRDOY, YRDOY0
+      INTEGER NLayr, INCDAT, RUN, YEAR, YRDOY, YRDOY0, NLayers
       LOGICAL FEXIST, FIRST
 
-      INTEGER NVars, I, L
-      REAL, DIMENSION(3) :: SoilDepth
-      REAL RootingDepth, Fraction
-
-!     List of variables to output:
-      REAL LAID   !Leaf area index
-      REAL NUPC   !N uptake, cumulative seasonal, kg/ha
-      REAL ESAA   !Soil evaporation, daily, mm
-      REAL EPAA   !Plant transpiration, daily, mm
-      REAL NMNC   !Net N mineralization, cumulative, kg/ha
-      REAL SCTD   !Soil organic C, kg/ha
-      REAL SNTD   !Soil organic N, kg/ha
-      REAL SWD1   !Soil water content at specified depth, mm3/mm3
-      REAL SWD2   !Soil water content at specified depth, mm3/mm3
-      REAL SWD3   !Soil water content at specified depth, mm3/mm3
-      REAL SNO3D  !Soil nitrate content to rooting depth, kg/ha
-      REAL SNH4D  !Soil ammonium content to rooting depth, kg/ha
+      INTEGER NVars, I, L, YR, iMON, NDAY
 
 !     Variables needed for computation of output variables:
-      REAL, DIMENSION(NL) :: SW   !used to calculate SWD1, SWD2, SWD3
-      REAL, DIMENSION(NL) :: SNO3 !used to calculate SNO3D
-      REAL, DIMENSION(NL) :: SNH4 !used to calculate SNH4D
-      REAL, DIMENSION(NL) :: DS   !depth to bottom of soil layer (cm)
+      REAL, DIMENSION(NL) :: SON, SOC, SW   
+      REAL, DIMENSION(NL) :: DS, DLAYR   
+      REAL TSW, TDEP, SWAVG, QCO2hum, QCO2res, QNhum, QNres
+      REAL SOCtop, SONtop
+
+!     Site ID
+      CHARACTER*4 SITEID, TRTNAME
 
       TYPE (ControlType) CONTROL
       TYPE (SoilType) SOILPROP
@@ -72,36 +61,31 @@
 !***********************************************************************
       IF (DYNAMIC == SEASINIT) THEN
 !-----------------------------------------------------------------------
-        NVars = 12
+        NVars = 7
         HeaderTxt = '         '
         FormatTxt = '         '
 
 !       Initialization values printed for seasinit represent end of previous day 
         YRDOY0 = INCDAT(YRDOY,-1)
         CALL YR_DOY(YRDOY0, YEAR, DOY)
-        
-!       Depths to report soil variables
-        SoilDepth(1) = 5.   !cm (SW1D)
-        SoilDepth(2) = 30.  !cm (SW2D)
-        SoilDepth(3) = 90.  !cm (SW3D)
-        RootingDepth = 100. !cm (SNO3D and SNH4D)
-        
+
+!-----------------------------------------------------------------------
 !       Initialize headers and output formats, set everything to zero for now.
-        HeaderTxt(1) ='      LAID' ; FormatTxt(1) = 'F10.2'; LAID  = 0.0
-        HeaderTxt(2) ='      NUPC' ; FormatTxt(2) = 'F10.1'; NUPC  = 0.0
-        HeaderTxt(3) ='      ESAA' ; FormatTxt(3) = 'F10.2'; ESAA  = 0.0
-        HeaderTxt(4) ='      EPAA' ; FormatTxt(4) = 'F10.2'; EPAA  = 0.0
-        HeaderTxt(5) ='      NMNC' ; FormatTxt(5) = 'F10.1'; NMNC  = 0.0
-        HeaderTxt(6) ='      SWD1' ; FormatTxt(6) = 'F10.3'; SWD1  = 0.0
-        HeaderTxt(7) ='      SWD2' ; FormatTxt(7) = 'F10.3'; SWD2  = 0.0
-        HeaderTxt(8) ='      SWD3' ; FormatTxt(8) = 'F10.3'; SWD3  = 0.0
-        HeaderTxt(9) ='     SNO3D' ; FormatTxt(9) = 'F10.2'; SNO3D = 0.0
-        HeaderTxt(10)='     SNH4D' ; FormatTxt(10)= 'F10.2'; SNH4D = 0.0
-        HeaderTxt(11)='      SCTD' ; FormatTxt(11)= 'F10.0'; SCTD  = 0.0
-        HeaderTxt(12)='      SNTD' ; FormatTxt(12)= 'F10.0'; SNTD  = 0.0
-     
+        HeaderTxt(1) ='       SOC' ; FormatTxt(1) = 'F10.0'
+        HeaderTxt(2) ='       SON' ; FormatTxt(2) = 'F10.1'
+        HeaderTxt(3) ='   QCO2hum' ; FormatTxt(3) = 'F10.2'
+        HeaderTxt(4) ='   QCO2res' ; FormatTxt(4) = 'F10.2'
+        HeaderTxt(5) ='     QNhum' ; FormatTxt(5) = 'F10.2'
+        HeaderTxt(6) ='     QNres' ; FormatTxt(6) = 'F10.2'
+        HeaderTxt(7) =' SoilW.layer1' ; FormatTxt(7) = 'F13.3'
+
+        QCO2hum = 0.0
+        QCO2res = 0.0
+        QNhum = 0.0
+        QNres = 0.0
+
 !       Build the format strings and the header text for ASCII and CSV
-        FMT_STRING_A = "(I5,I4,I6," // TRIM(FormatTxt(1))   !Format for outputs
+        FMT_STRING_A = "(I5,I4,I6," // TRIM(FormatTxt(1)) 
         HDR_String_A = HeaderTxt(1) !ASCII header line
         HDR_String_C = ADJUSTL(HeaderTxt(1)) !CSV header line
         DO I = 2, NVars
@@ -111,7 +95,7 @@
      &                   TRIM(ADJUSTL(HeaderTxt(I)))
         ENDDO
         FMT_STRING_A = TRIM(FMT_STRING_A) // ")"
-        WRITE(FMT_STRING_C,'(A,I2,A)') "(", NVars+7, "(g0,','),g0)"
+        WRITE(FMT_STRING_C,'(A,I2,A)') "(", NVars+5, "(g0,','),g0)"
 
 !       ----------------------------------------------------
 !       Open ASCII file and write headers
@@ -142,70 +126,71 @@
      &        IOSTAT = ERRNUM)
           ENDIF
           WRITE(LUN2,'(A,A)') 
-     &    "RUN,EXP,TRTNUM,ROTNUM,REPNO,YEAR,DOY,DAS,",TRIM(HDR_String_C)
+     &      "Model,id_site,id_season,id_treatment,Date",
+     &      TRIM(HDR_String_C)
         ENDIF
+
+!       Use treatment name to get site ID and Low Input treatment
+        SITEID  = CONTROL % TITLET(1:4)
+        TRTNAME = CONTROL % TITLET(6:9)
+
+!       For Low input systems, the layer depth is dependent on location
+        SELECT CASE(SITEID)
+        CASE ('ICGA','ZIMU')
+          NLayers = 3
+        CASE ('KEMA','KEEM')
+          NLayers = 2
+        CASE DEFAULT
+          NLayers = 0
+          MSG(1) = "Wrong site ID."
+          CALL WARNING(1, ERRKEY, MSG)
+        END SELECT
+
+        CALL GET(SOILPROP)
+        NLayr = SOILPROP % NLayr
+        DS    = SOILPROP % DS
+
       ENDIF  !End of seasinit section
 
 !***********************************************************************
 !***********************************************************************
 !     Daily OUTPUT (do for all values of DYNAMIC)
 !***********************************************************************
+!     Today's date
+      CALL ETAD_NAILUJ (YRDOY, YR, iMON, NDAY)
+      WRITE(DATETXT, '(I4,"-",I2.2,"-",I2.2)') YR, iMON, NDAY
+
 !     Get daily values
-      CALL GET(SOILPROP)
-      NLayr = SOILPROP % NLayr
-      DS    = SOILPROP % DS
+      CALL GET('ORGC', 'SOC', SOC)
+      CALL GET('ORGC', 'SON', SON)
+      CALL GET('WATER', 'SW', SW) 
 
-      CALL GET('PLANT', 'LAID',    LAID) 
-      CALL GET('PLANT', 'NUPTAKE', NUPC) 
-      CALL GET('SPAM',  'ES',      ESAA) 
-      CALL GET('SPAM',  'EP',      EPAA) 
-      CALL GET('NITR',  'NET_MIN', NMNC) 
-      CALL GET('NITR',  'SNO3',    SNO3)
-      CALL GET('NITR',  'SNH4',    SNH4)
-      CALL GET('WATER', 'SW',      SW) 
-      CALL GET('ORGC',  'SCTD',    SCTD)
-      CALL GET('ORGC',  'SNTD',    SNTD)
-      
 !     Extract soil water at specified depths
-      SWD1 = SW(1)
-      SWD2 = SW(1)
-      SWD3 = SW(1)
-      DO L = 2, NLayr
-        IF (DS(L-1) .LT. SoilDepth(1)) SWD1 = SW(L)
-        IF (DS(L-1) .LT. SoilDepth(2)) SWD2 = SW(L)
-        IF (DS(L-1) .LT. SoilDepth(3)) SWD3 = SW(L)
+      TSW    = 0.0
+      SOCtop = 0.0
+      SONtop = 0.0
+
+      DO L = 1, NLayers
+        TSW = TSW + SW(L) * DLAYR(L)
+        TDEP = DS(L)
+        SOCtop = SOCtop + SOC(L)
+        SONtop = SONtop + SON(L)
       ENDDO
 
-!     Extract SNO3 and SNH4 through rooting depth
-      SNO3D = SNO3(1)
-      SNH4D = SNH4(1)
-      DO L = 2, NLAYR
-        IF (DS(L) < RootingDepth) THEN
-!         Entire layer is within rooting depth
-          SNO3D = SNO3D + SNO3(L)
-          SNH4D = SNH4D + SNH4(L)
-        ELSEIF (DS(L-1) < RootingDepth) THEN
-!         Partial layer in rooting depth
-          Fraction = (RootingDepth - DS(L-1)) / (DS(L) - DS(L-1))
-          SNO3D = SNO3D + SNO3(L) * Fraction
-          SNH4D = SNH4D + SNH4(L) * Fraction
-        ELSE
-!         Layer is entirely below rooting depth
-          EXIT
-        ENDIF
-      ENDDO
+      SWAVG = TSW / TDEP
+      SOCtop = SOCtop / 1000.   !t
+      SONtop = SONtop / 1000.   !t
 
 !     ----------------------------------------------------
 !     ASCII format output
-      WRITE(LUN1,TRIM(FMT_STRING_A)) YEAR, DOY, DAS, LAID, NUPC, ESAA, 
-     &    EPAA, NMNC, SWD1, SWD2, SWD3, SNO3D, SNH4D, SCTD, SNTD
+      WRITE(LUN1,TRIM(FMT_STRING_A)) YEAR, DOY, DAS, 
+     &    SOCtop, SONtop, QCO2hum, QCO2res, QNhum, QNres, SWAVG
      
 !     ----------------------------------------------------
 !     CSV format output
-      WRITE(LUN2,TRIM(FMT_STRING_C)) RUN, CONTROL%FILEX, CONTROL%TRTNUM,
-     &    CONTROL%ROTNUM, CONTROL%REPNO, YEAR, DOY, DAS, 
-     &    LAID, NUPC, ESAA, EPAA, NMNC, 
-     &    SWD1, SWD2, SWD3, SNO3D, SNH4D, SCTD, SNTD
+      WRITE(LUN2,TRIM(FMT_STRING_C)) "CE1", SITEID, 
+     &    CONTROL%ROTNUM, TRTNAME, DATETXT,
+     &    SOCtop, SONtop, QCO2hum, QCO2res, QNhum, QNres, SWAVG
 
 !***********************************************************************
 !***********************************************************************
